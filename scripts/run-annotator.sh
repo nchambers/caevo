e408f6ad23c2af0dd8edc1746de6ce45e7c66995
Nate Chambers <nchamber@usna.edu>
2013-06-06 10:52:49 -0400

    Scripts for running annotator

---
+++ scripts/run-annotator.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+
+if [ $# -lt 1 ]; then
+    echo "run-annotator.sh <html-file> [progress.txt]"
+    exit;
+fi
+
+#export JARS=$1
+#export INPUTDIR=$2
+export JAR=annotator.jar
+
+# Run it.
+export CLASSPATH=.:$JAR
+java timesieve.Annotator $1 $2
