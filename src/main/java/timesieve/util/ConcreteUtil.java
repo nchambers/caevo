daee9c6d8332f3d5fcd57482105bb0eca88699af
Bill McDowell <forkunited@gmail.com>
2013-06-24 09:08:33 -0400

    More progress on Concrete conversion

--- src/main/java/timesieve/util/ConcreteUtil.java
+++ src/main/java/timesieve/util/ConcreteUtil.java
@@ -7,12 +7,18 @@ import java.util.HashMap;
 import java.util.Map.Entry;
 
 import timesieve.*;
-import timesieve.TextEvent.POLARITY;
 import timesieve.tlink.TLink;
 import edu.jhu.hlt.concrete.Concrete.*;
 import edu.jhu.hlt.concrete.Concrete.Sentence;
 import edu.jhu.hlt.concrete.Concrete.TokenTagging.TaggedToken;
+import edu.jhu.hlt.concrete.Concrete.Tokenization.Kind;
+import edu.jhu.hlt.concrete.util.IdUtil;
 import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.Label;
+import edu.stanford.nlp.trees.LabeledScoredTreeFactory;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreeFactory;
+import edu.stanford.nlp.trees.TypedDependency;
 
 /**
  * Methods to help with using Concrete (https://github.com/hltcoe/concrete) data sources
@@ -415,6 +421,7 @@ public class ConcreteUtil {
 				if (sentence.hasTextSpan())
 					sText = commText.substring(sentence.getTextSpan().getStart(), sentence.getTextSpan().getEnd());
 				
+				Tokenization tokenization = null;
 				if (sentence.getTokenizationCount() > 0) {
 					List<Tokenization> toks = sentence.getTokenizationList();
 					for (Tokenization tok : toks) {
@@ -428,7 +435,7 @@ public class ConcreteUtil {
 									continue;
 								
 								if (token.hasText())
-									sToken.setOriginalText(token.getText());
+									sToken.setValue(token.getText());
 								
 								if (token.hasTextSpan()) {
 									sToken.setBeginPosition(token.getTextSpan().getStart());
@@ -463,17 +470,22 @@ public class ConcreteUtil {
 								}
 							}
 							
+							tokenization = tok;
+							
 							break;
 						}
 					}				
 				}
-					
-				if (sentence.getParseCount() > 0) {
-					/* FIXME */
+				
+				if (tokenization != null && sentence.getParseCount() > 0) {
+						Tree parseTree = ConcreteUtil.communicationParseToTree(sentence.getParse(0), tokenization);
+						if (parseTree != null)
+							sParse = parseTree.toString();
 				}
 				
 				if (sentence.getDependencyParseCount() > 0) {
 					/* FIXME */
+					
 				}
 					
 				info.addSentence(ConcreteUtil.filenameFromCommunication(comm), 
@@ -490,13 +502,435 @@ public class ConcreteUtil {
 		
 	}
 	
+	private static Tree communicationParseToTree(Parse parse, Tokenization tokens) {
+		/* FIXME: Outstanding issues and questions
+		 * - Why is parse stored as a string
+		 * 		- Used Tree.toString to get this... is this the right thing to do?
+		 * - Used LabeledScoredTreeFactory
+		 * - Assumed that sentences are not extremely long, making ginormous parse trees
+		 * 		- Used straightforward recursive implementation which could cause stack overflow
+		 * - Only added token ref sequence to leaf constituents
+		 * - Ignored the head constituent designation in converting to tree
+		 * - Just used first tokenization for now... and threw exception if it doesn't line up
+		 * - Assumed I should just concatenate the tokens in token ref seq to make the leaf strings
+		 * - Assumed the number of tokens in a tokenization is small, and just iterated to find tokens by token id
+		 */
+		
+		if (!parse.hasRoot())
+			return null;
+		TreeFactory treeFactory = new LabeledScoredTreeFactory();
+		return ConcreteUtil.communicationParseToTreeHelper(parse.getRoot(), tokens, treeFactory);
+	}
+	
+	private static Tree communicationParseToTreeHelper(Parse.Constituent constituent, Tokenization tokens, TreeFactory factory) {
+		if (constituent.getChildCount() == 0) { // Leaf
+			TokenRefSequence tokenRefSeq = constituent.getTokenSequence();
+			if (tokenRefSeq.hasTokenization() && !tokenRefSeq.getTokenization().equals(tokens.getUuid()))
+				throw new IllegalArgumentException();
+			
+			List<Integer> tokenRefs = tokenRefSeq.getTokenIdList();
+			StringBuilder leafLabel = new StringBuilder();
+			for (Integer tokenRef : tokenRefs) {
+				for (Token token : tokens.getTokenList())
+					if (token.hasText() && token.hasTokenId() && token.getTokenId() == tokenRef)
+						leafLabel.append(token.getText());
+			}
+			
+			return factory.newLeaf(leafLabel.toString());
+		} else { // Not leaf
+			List<Parse.Constituent> children = constituent.getChildList();
+			List<Tree> treeChildren = new ArrayList<Tree>();
+			for (Parse.Constituent child : children) {
+				treeChildren.add(ConcreteUtil.communicationParseToTreeHelper(child, tokens, factory));
+			}
+			return factory.newTreeNode(constituent.getTag(), treeChildren);
+		}
+	}
+	
 	/*
 	 *  Methods for transforming info file to communication
 	 */
 	
-	public static Communication infoFileToCommunication(InfoFile info) {
-		/* FIXME */
+	public static List<Communication> infoFileToCommunication(String corpusName, InfoFile info) {
+		/* FIXME: Unresolved issues and questions
+		 * 	- Used IdUtil to generate UUID... is this the right thing to do?
+		 *  - Communications require a corpus name in the identifier... should InfoFile also have this
+		 *  	- Right now, I just have it passed into the function separately from the InfoFile
+		 *  	- And InfoFile file names line up with Communication GUID communicationIds
+		 *  - Why is KnowledgeGraph required in Communication?
+		 *  - I left AnnotationMetaData fields blank for now... should these be filled in?
+		 *  	- For example, what tool outputs the sentence segmentation?
+		 */
 		
-		return null;
+		List<String> files = info.getFiles();
+		List<Communication> communications = new ArrayList<Communication>();
+		
+		for (String file : files) {
+			List<Sentence> sentences = new ArrayList<Sentence>();
+			String text = ConcreteUtil.textDataFromInfoFile(info, file, sentences);
+			
+			List<Entity> timexEntities = new ArrayList<Entity>();
+			List<EntityMention> timexEntityMentions = new ArrayList<EntityMention>();
+			HashMap<String, UUID> timexIdMap = new HashMap<String, UUID>();
+			ConcreteUtil.timexEntitiesFromInfoFile(info, file, timexEntities, timexEntityMentions, timexIdMap);
+
+			List<Situation> eventSituations = new ArrayList<Situation>();
+			List<SituationMention> eventSituationMentions = new ArrayList<SituationMention>();
+			HashMap<String, UUID> eventIdMap = new HashMap<String, UUID>();		
+			ConcreteUtil.eventSituationsFromInfoFile(info, file, eventSituations, eventSituationMentions, eventIdMap);
+			
+			List<Situation> tlinkSituations = new ArrayList<Situation>();
+			ConcreteUtil.tlinkSituationsFromInfoFile(info, file, tlinkSituations, timexIdMap, eventIdMap);
+																
+			communications.add(
+				Communication.newBuilder()
+					.setUuid(IdUtil.generateUUID())
+					.setGuid(
+						CommunicationGUID.newBuilder()
+							.setCorpusName(corpusName)
+							.setCommunicationId(file)
+							.build()
+					)
+					.setText(text)
+					.addSectionSegmentation(
+						SectionSegmentation.newBuilder()
+						.setUuid(IdUtil.generateUUID())
+						.addSection(
+							Section.newBuilder()
+								.setUuid(IdUtil.generateUUID())
+								.setTextSpan(
+									TextSpan.newBuilder()
+										.setStart(0)
+										.setEnd(text.length())
+										.build()
+								)
+								.addSentenceSegmentation(
+									SentenceSegmentation.newBuilder()
+										.setUuid(IdUtil.generateUUID())
+										.addAllSentence(sentences)
+								)
+								.build()
+						)
+						.build()
+					)
+					.addEntityMentionSet(
+						EntityMentionSet.newBuilder()
+							.setUuid(IdUtil.generateUUID())
+							.addAllMention(timexEntityMentions)
+							.build()
+					)
+					.addEntitySet(
+						EntitySet.newBuilder()
+							.setUuid(IdUtil.generateUUID())
+							.addAllEntity(timexEntities)
+							.build()
+					)
+					.addSituationMentionSet(
+						SituationMentionSet.newBuilder()
+							.setUuid(IdUtil.generateUUID())
+							.addAllMention(eventSituationMentions)
+							.build()
+					)
+					.addSituationSet(
+						SituationSet.newBuilder()
+							.setUuid(IdUtil.generateUUID())
+							.addAllSituation(eventSituations)
+							.addAllSituation(tlinkSituations)
+							.build()
+					)
+					.build()
+			);
+		}
+		
+		return communications;
+	}
+	
+	private static String textDataFromInfoFile(InfoFile info, String file, List<Sentence> sentences) {
+		/* FIXME: Outstanding issues and questions
+		 * - Still need to do dependency and parse conversion
+		 * - Assumed token text is just the coreLabel's "value" (instead of "original text")
+		 * 	- Not sure how begin and end position in core label relate to this?  Are they what should be used by text span?
+		 * - Used iterator to fill in token_ids
+		 * - It seems like InfoFile should have a method for returning just the text of the file
+		 *    - Instead of just in terms of CoreLabels and sentences...
+		 *    - It looks like the sentences in the example info file are constructed incorrectly (there is a space before each period)
+		 *    - I reconstructed text by taking each CoreLabels middle value and last value (since first and last
+		 *      values of consecutive CoreLabels overlap
+		 *  - Why are the dependency trees not part of the infofile sentence objects?
+		 *  - Currently, I assume that all InfoFile sentences have parse and dependency trees, is that okay?
+		 */
+		
+		StringBuilder text = new StringBuilder();
+		List<timesieve.Sentence> infoSentences = info.getSentences(file);
+		List<String> dependencyStrs = info.getDependencies(file);
+		
+		if (infoSentences.size() != dependencyStrs.size())
+			throw new IllegalArgumentException();
+		
+		for (int i = 0; i < infoSentences.size(); i++) {
+			List<Token> tokens = new ArrayList<Token>();
+			List<TaggedToken> lemmas = new ArrayList<TaggedToken>();
+			List<TaggedToken> nerTags = new ArrayList<TaggedToken>();
+			List<TaggedToken> posTags = new ArrayList<TaggedToken>();
+			
+			timesieve.Sentence infoSentence = infoSentences.get(i);
+			List<CoreLabel> coreLabels = infoSentence.tokens();
+			int tokenId = 0;
+			for (CoreLabel coreLabel : coreLabels) {
+				if (coreLabel.lemma() != null) {
+					lemmas.add(
+						TaggedToken.newBuilder()
+							.setTag(coreLabel.lemma())
+							.setTokenId(tokenId)
+							.build()
+					);
+				}
+				
+				if (coreLabel.ner() != null) {
+					nerTags.add(
+						TaggedToken.newBuilder()
+							.setTag(coreLabel.ner())
+							.setTokenId(tokenId)
+							.build()
+					);					
+				}
+				
+				if (coreLabel.tag() != null) {
+					posTags.add(
+						TaggedToken.newBuilder()
+							.setTag(coreLabel.tag())
+							.setTokenId(tokenId)
+							.build()
+					);						
+				}
+				
+				tokens.add(
+					Token.newBuilder()
+						.setTokenId(tokenId)
+						.setTextSpan(
+							TextSpan.newBuilder()
+								.setStart(coreLabel.beginPosition())
+								.setEnd(coreLabel.endPosition())
+								.build()
+					  )
+						.setText(coreLabel.value())
+						.build()
+				);
+				
+				tokenId++;
+			}
+			
+			//Tree parseTree = Tree.valueOf(parseStr);
+			Parse parse = null;
+			/* FIXME: Do Parse */
+			
+			//TypedDependency dependencyTree = TreeOperator.stringToDependency(dependencyStr);
+			DependencyParse dependencyParse = null;
+			/* FIXME: Do Dependency */
+			
+			text.append(infoSentence.sentence());
+			
+			sentences.add(
+				Sentence.newBuilder()
+					.setUuid(IdUtil.generateUUID())
+					.setTextSpan(
+						TextSpan.newBuilder()
+							.setStart(text.length()-infoSentence.sentence().length())
+							.setEnd(text.length())
+							.build()
+					)
+					.addTokenization(
+						Tokenization.newBuilder()
+							.setUuid(IdUtil.generateUUID())
+							.setKind(Kind.TOKEN_LIST)
+							.addPosTags(
+								TokenTagging.newBuilder()
+									.setUuid(IdUtil.generateUUID())
+									.addAllTaggedToken(posTags)
+									.build()
+							)
+							.addNerTags(
+								TokenTagging.newBuilder()
+									.setUuid(IdUtil.generateUUID())
+									.addAllTaggedToken(nerTags)
+									.build()
+							)
+							.addLemmas(
+									TokenTagging.newBuilder()
+										.setUuid(IdUtil.generateUUID())
+										.addAllTaggedToken(lemmas)
+										.build()
+							)
+							.addAllToken(tokens)
+							.build()
+					)
+					.addParse(parse)
+					.addDependencyParse(dependencyParse)
+					.build()
+			);
+		}
+		
+		return text.toString();
+	}
+	
+	private static void timexEntitiesFromInfoFile(InfoFile info, String file, List<Entity> entities, List<EntityMention> entityMentions, HashMap<String, UUID> timexIdMap) {
+		/* FIXME: Outstanding issues and questions
+		 * 	- This makes one entity mention per timex entity (even if same time has multiple mentions)
+		 *  - Left out TextSpan and HeadIndex (timex doesn't have them)
+		 *  - Mapped duration to time for now
+		 *  - Why no enum for timex types?
+		 */
+		
+		List<Timex> timexes = info.getTimexes(file);
+		for (Timex timex : timexes) {
+			UUID entityMentionId = IdUtil.generateUUID();
+			UUID entityId = IdUtil.generateUUID();
+			
+			timexIdMap.put(timex.tid(), entityId);
+			
+			entityMentions.add(
+				EntityMention.newBuilder()
+					.setUuid(entityMentionId)
+					.setText(timex.text())
+					.setSentenceIndex(timex.sid())
+					.setEntityType((timex.type().equals("DATE") ? Entity.Type.DATE : Entity.Type.TIME))
+					.build()
+			);
+			
+			entities.add(
+				Entity.newBuilder()
+					.setUuid(entityId)
+					.setEntityType((timex.type().equals("DATE") ? Entity.Type.DATE : Entity.Type.TIME))
+					.setCanonicalName(timex.value())
+					.addMention(entityMentions.get(entityMentions.size() - 1).getUuid())
+					.build()
+			);
+		}
+	}
+	
+	private static void eventSituationsFromInfoFile(InfoFile info, String file, List<Situation> situations, List<SituationMention> situationMentions, HashMap<String, UUID> eventIdMap) {
+		/* FIXME: Outstanding issues and questions
+		 * - Event type and state type and arguments left out for now
+		 * - Left out arguments, tokens, class, entities, situationkindlemma etc.
+		 * - Situation mentions take either textspan(s) or pointers to tokens.
+		 * 	- Neither of these are directly available through TextEvent 
+		 * - Situation type is either state or event for now... (but should map to the class)
+		 * 	- Why are textEvent enums converted to strings?
+		 * - Assumed all justification types should be direct_mention
+		 * - Only added polarity to SituationMention and not Situation.
+		 * 	- Seems odd for situations to also have polarity
+		 */
+		
+		List<TextEvent> textEvents = info.getEvents(file);
+		for (TextEvent textEvent : textEvents) {
+			UUID situationId = IdUtil.generateUUID();
+			UUID situationMentionId = IdUtil.generateUUID();
+			
+			Situation.Type situationType = Situation.Type.EVENT;
+			if (textEvent.getClass() != null && textEvent.getClass().equals("STATE"))
+				situationType = Situation.Type.STATE;
+			
+			
+			SituationMention.Builder situationMentionBuilder =
+				SituationMention.newBuilder()
+					.setUuid(situationMentionId)
+					.setSituationType(situationType);
+			
+			if (textEvent.getPolarity() != null) {
+				situationMentionBuilder.setPolarity(
+					textEvent.getPolarity().equals("POS") ? 
+						Situation.Polarity.POSITIVE_POLARITY 
+					: Situation.Polarity.NEGATIVE_POLARITY);
+			}
+			
+			SituationMention situationMention = situationMentionBuilder.build();
+			
+			
+			eventIdMap.put(textEvent.id(), situationId);
+			
+			situationMentions.add(situationMention);
+			
+			situations.add(
+				Situation.newBuilder()
+					.setUuid(situationId)
+					.addJustification(
+						Situation.Justification.newBuilder()
+							.setMention(situationMention.getUuid())
+							.setJustificationType(Situation.Justification.Type.DIRECT_MENTION)
+							.build()
+					)
+					.build()
+			);
+		}
+	}
+	
+	private static void tlinkSituationsFromInfoFile(InfoFile info, String file, List<Situation> situations, HashMap<String, UUID> timexIdMap, HashMap<String, UUID> eventIdMap) {
+		/* FIXME: Outstanding issues and questions
+		 * - Didn't add any justifications 
+		 * - If TLink is not one of the 6 acceptable types, it's just counted as vague.  Should I do something different?
+		 * - If first or second event/time invalid, currently just skip over the link
+		 */
+		
+		List<TLink> tlinks = info.getTlinks(file);
+		for (TLink tlink : tlinks) {			
+			Situation.TemporalFactType temporalFactType = Situation.TemporalFactType.VAGUE_TEMPORAL_FACT;
+			if (tlink.relation() == TLink.TYPE.AFTER)
+				temporalFactType = Situation.TemporalFactType.AFTER_TEMPORAL_FACT;
+			else if (tlink.relation() == TLink.TYPE.BEFORE)
+				temporalFactType = Situation.TemporalFactType.BEFORE_TEMPORAL_FACT;
+			else if (tlink.relation() == TLink.TYPE.INCLUDES)
+				temporalFactType = Situation.TemporalFactType.INCLUDES_TEMPORAL_FACT;
+			else if (tlink.relation() == TLink.TYPE.IS_INCLUDED)
+				temporalFactType = Situation.TemporalFactType.IS_INCLUDED_BY_TEMPORAL_FACT;
+			else if (tlink.relation() == TLink.TYPE.SIMULTANEOUS)
+				temporalFactType = Situation.TemporalFactType.SIMULTANEOUS_TEMPORAL_FACT;
+			
+			Situation.Argument.ValueType firstArgumentType = Situation.Argument.ValueType.UNKNOWN_ARG;
+			UUID firstArgumentId = null;
+			if (timexIdMap.containsKey(tlink.event1())) {
+				firstArgumentType = Situation.Argument.ValueType.ENTITY_ARG;
+				firstArgumentId = timexIdMap.get(tlink.event1());
+			} else if (eventIdMap.containsKey(tlink.event1())) {
+				firstArgumentType = Situation.Argument.ValueType.SITUATION_ARG;			
+				firstArgumentId = eventIdMap.get(tlink.event1());
+			} else {
+				continue;
+			}
+			
+			Situation.Argument.ValueType secondArgumentType = Situation.Argument.ValueType.UNKNOWN_ARG;
+			UUID secondArgumentId = null;
+			if (eventIdMap.containsKey(tlink.event2())) {
+				secondArgumentType = Situation.Argument.ValueType.ENTITY_ARG;
+				secondArgumentId = timexIdMap.get(tlink.event2());
+			} else if (eventIdMap.containsKey(tlink.event1())) {
+				secondArgumentType = Situation.Argument.ValueType.SITUATION_ARG;			
+				secondArgumentId = eventIdMap.get(tlink.event2());
+			} else {
+				continue;
+			}
+			
+			situations.add(
+				Situation.newBuilder()
+					.setUuid(IdUtil.generateUUID())
+					.setSituationType(Situation.Type.TEMPORAL_FACT)
+					.addArgument(
+						Situation.Argument.newBuilder()
+							.setRole(Situation.Argument.Role.RELATION_SOURCE_ROLE)
+							.setValueType(firstArgumentType)
+							.setValue(firstArgumentId)
+							.build()
+					)
+					.addArgument(
+							Situation.Argument.newBuilder()
+								.setRole(Situation.Argument.Role.RELATION_TARGET_ROLE)
+								.setValueType(secondArgumentType)
+								.setValue(secondArgumentId)
+								.build()
+					)
+					.setTemporalFactType(temporalFactType)
+					.setConfidence((float)tlink.relationConfidence())
+					.build()
+			);
+		}
 	}
 }
