c0e9134af213f7ce3178af64186cfcd131f76fb4
Nate Chambers <nchambers@gmail.com>
2013-08-01 16:57:07 -0400

    New code to find all event pairs that are dominated, not just immediate gov-dep pairs.

--- src/main/java/timesieve/sieves/RepEventGovEvent.java
+++ src/main/java/timesieve/sieves/RepEventGovEvent.java
@@ -9,9 +9,12 @@ import timesieve.SieveDocument;
 import timesieve.SieveDocuments;
 import timesieve.SieveSentence;
 import timesieve.TextEvent;
+import timesieve.TextEvent.Aspect;
+import timesieve.TextEvent.Tense;
 import timesieve.tlink.EventEventLink;
 import timesieve.tlink.TLink;
 import timesieve.tlink.TLink.Type;
+import timesieve.util.Ling;
 import edu.stanford.nlp.trees.TypedDependency;
 
 /**
@@ -36,6 +39,7 @@ public class RepEventGovEvent implements Sieve {
 				indexToEvent.put(event.getIndex(), event);
 			}
 			
+			// Find all event-event pairs with a typed dependency where one is governor of the other.
 			for (TypedDependency dep : deps) {
 				TextEvent govEvent = indexToEvent.get(dep.gov().index());
 				TextEvent depEvent = indexToEvent.get(dep.dep().index());
@@ -47,138 +51,178 @@ public class RepEventGovEvent implements Sieve {
 							// in our annotations, AFTER gives (p=0.50 5 of 10) and VAGUE
 							// gives (p=0.40)
 						} else {
-							switch (govEvent.getTense()) {
-							
-							// gov=PAST
-							case PAST:
-								switch (depEvent.getTense()) {
-
-								// gov=PAST, dep=PAST
-								case PAST:
-									switch (depEvent.getAspect()) {
-									case NONE: // p=0.95 20 of 21 (ignoring VAGUE)
-									case PERFECTIVE: // p=1.00 1 of 1 (ignoring VAGUE)
-										relation = Type.AFTER;
-										break;
-									case PROGRESSIVE: // p=1.00 1 of 1
-										relation = Type.IS_INCLUDED;
-										break;
-									// never occurs in training data
-									case PERFECTIVE_PROGRESSIVE:
-									case IMPERFECTIVE:
-									case IMPERFECTIVE_PROGRESSIVE:
-										break;
-									}
-									break;
-
-								// gov=PAST, dep=PRESENT
-								case PRESENT:
-									switch (depEvent.getAspect()) {
-									case PERFECTIVE: // p=1.00 6 of 6 (ignoring VAGUE)
-										relation = Type.AFTER;
-										break;
-									case PROGRESSIVE: // p=1.00 4 of 4 (ignoring VAGUE)
-									case PERFECTIVE_PROGRESSIVE: // p=1.00 2 of 2 (ignoring VAGUE)
-										relation = Type.IS_INCLUDED;
-										break;
-									case NONE: // p=0.71 22 of 31, but no linguistic reason why
-										// relation = Type.AFTER;
-									case IMPERFECTIVE: // p=0.50 5 of 10, but no linguistic reason
-										// relation = Type.IS_INCLUDED;
-										break;
-
-									// never occurs in training data
-									case IMPERFECTIVE_PROGRESSIVE:
-										break;
-									}
-									break;
-
-								// gov=PAST, dep=FUTURE
-								case FUTURE: // p=1.00 5 of 5 (ignoring VAGUE)
-									relation = Type.BEFORE;
-									break;
-
-								// gov=PAST, dep=NONE
-								case NONE: // p=0.70 14 of 20, but no linguistic reason why
-									// relation = Type.BEFORE;
-									break;
-
-								// never occurs in training data
-								case INFINITIVE:
-								case PASSIVE:
-								case PASTPART:
-								case PRESPART:
-									break;
-								}
-								break;
-
-							// gov=PRESENT
-							case PRESENT:
-								switch (depEvent.getTense()) {
-
-								// gov=PRESENT, dep=PAST
-								case PAST: // not in data, but makes linguistic sense
-									relation = Type.AFTER;
-									break;
-
-								// gov=PRESENT, dep=PRESENT
-								case PRESENT:
-									switch (depEvent.getAspect()) {
-									case PERFECTIVE: // p=1.00 1 of 1 (ignoring VAGUE)
-										relation = Type.AFTER;
-										break;
-
-									case NONE: // p=1.00 2 of 2, but no linguistic reason why
-										// relation = Type.IS_INCLUDED;
-										break;
-
-									// never occurs in training data
-									case PROGRESSIVE:
-									case PERFECTIVE_PROGRESSIVE:
-									case IMPERFECTIVE:
-									case IMPERFECTIVE_PROGRESSIVE:
-										break;
-									}
-									break;
-
-								// gov=PRESENT, dep=FUTURE
-								case FUTURE: // p=1.00 2 of 2 (ignoring VAGUE)
-									relation = Type.BEFORE;
-									break;
-
-								// gov=PRESENT, dep=... only 0-1 occurrences in training data
-								case NONE:
-								case INFINITIVE:
-								case PASSIVE:
-								case PASTPART:
-								case PRESPART:
-									break;
-								}
-								break;
-
-							// gov=... only 0-1 occurrences in training data
-							case NONE:
-							case PRESPART:
-							case FUTURE:
-							case INFINITIVE:
-							case PASSIVE:
-							case PASTPART:
-								break;
-							}
+							relation = getRelation(govEvent.getTense(), govEvent.getAspect(), depEvent.getTense(), depEvent.getAspect());
 						}
+						
+						if (relation != null) {
+							proposed.add(new EventEventLink(govEvent.getEiid(), depEvent.getEiid(), relation));
+							System.out.println("GOVDEP:\t\t" + new EventEventLink(govEvent.getEiid(), depEvent.getEiid(), relation));
+						}
+					}
+				}
+			}
+
+			// Find all event-event pairs where one syntactically dominates the other.
+			for( TextEvent govEvent : events ) {
+				List<TextEvent> dominatedEvents = Ling.getAllDominatedEvents(sent, govEvent);
+				for( TextEvent depEvent : dominatedEvents ) {
+					if (govEvent.getTheClass().equals(TextEvent.Class.REPORTING)) {
+						Type relation = null;
+						if (depEvent.getTheClass().equals(TextEvent.Class.REPORTING)) {
+							// no clear pattern for relations between speech events;
+							// in our annotations, AFTER gives (p=0.50 5 of 10) and VAGUE
+							// gives (p=0.40)
+						} else {
+							relation = getRelation(govEvent.getTense(), govEvent.getAspect(), depEvent.getTense(), depEvent.getAspect());
+						}
+
 						// no rules so far for for PRESENT or FUTURE since:
 						// only 5 instances of govTense=PRESENT in TimeBank
 						// only 1 instance of govTense=FUTURE in TimeBank
 						if (relation != null) {
 							proposed.add(new EventEventLink(govEvent.getEiid(), depEvent.getEiid(), relation));
+							System.out.println("DOMINATED:\t" + new EventEventLink(govEvent.getEiid(), depEvent.getEiid(), relation));
 						}
 					}
 				}
 			}
+
 		}
 		return proposed;
 	}
 
+	/**
+	 * Determine the event-event relation from just the tenses and aspects.
+	 * @param t1 Governing event's tense
+	 * @param a1 Governing event's aspect
+	 * @param t2 Dependent event's tense
+	 * @param a2 Dependent event's aspect
+	 * @return Relation from the rules, or null if no rule applied.
+	 */
+	private Type getRelation(Tense t1, Aspect a1, Tense t2, Aspect a2) {
+		Type relation = null;
+
+		switch (t1) {
+
+		// gov=PAST
+		case PAST:
+			switch (t2) {
+
+			// gov=PAST, dep=PAST
+			case PAST:
+				switch (a2) {
+				case NONE: // p=0.95 20 of 21 (ignoring VAGUE)
+				case PERFECTIVE: // p=1.00 1 of 1 (ignoring VAGUE)
+					relation = Type.AFTER;
+					break;
+				case PROGRESSIVE: // p=1.00 1 of 1
+					relation = Type.IS_INCLUDED;
+					break;
+					// never occurs in training data
+				case PERFECTIVE_PROGRESSIVE:
+				case IMPERFECTIVE:
+				case IMPERFECTIVE_PROGRESSIVE:
+					break;
+				}
+				break;
+
+				// gov=PAST, dep=PRESENT
+			case PRESENT:
+				switch (a2) {
+				case PERFECTIVE: // p=1.00 6 of 6 (ignoring VAGUE)
+					relation = Type.AFTER;
+					break;
+				case PROGRESSIVE: // p=1.00 4 of 4 (ignoring VAGUE)
+				case PERFECTIVE_PROGRESSIVE: // p=1.00 2 of 2 (ignoring VAGUE)
+					relation = Type.IS_INCLUDED;
+					break;
+				case NONE: // p=0.71 22 of 31, but no linguistic reason why
+					// relation = Type.AFTER;
+				case IMPERFECTIVE: // p=0.50 5 of 10, but no linguistic reason
+					// relation = Type.IS_INCLUDED;
+					break;
+
+					// never occurs in training data
+				case IMPERFECTIVE_PROGRESSIVE:
+					break;
+				}
+				break;
+
+				// gov=PAST, dep=FUTURE
+			case FUTURE: // p=1.00 5 of 5 (ignoring VAGUE)
+				relation = Type.BEFORE;
+				break;
+
+				// gov=PAST, dep=NONE
+			case NONE: // p=0.70 14 of 20, but no linguistic reason why
+				// relation = Type.BEFORE;
+				break;
+
+				// never occurs in training data
+			case INFINITIVE:
+			case PASSIVE:
+			case PASTPART:
+			case PRESPART:
+				break;
+			}
+			break;
+
+			// gov=PRESENT
+		case PRESENT:
+			switch (t2) {
+
+			// gov=PRESENT, dep=PAST
+			case PAST: // not in data, but makes linguistic sense
+				relation = Type.AFTER;
+				break;
+
+				// gov=PRESENT, dep=PRESENT
+			case PRESENT:
+				switch (a2) {
+				case PERFECTIVE: // p=1.00 1 of 1 (ignoring VAGUE)
+					relation = Type.AFTER;
+					break;
+
+				case NONE: // p=1.00 2 of 2, but no linguistic reason why
+					// relation = Type.IS_INCLUDED;
+					break;
+
+					// never occurs in training data
+				case PROGRESSIVE:
+				case PERFECTIVE_PROGRESSIVE:
+				case IMPERFECTIVE:
+				case IMPERFECTIVE_PROGRESSIVE:
+					break;
+				}
+				break;
+
+				// gov=PRESENT, dep=FUTURE
+			case FUTURE: // p=1.00 2 of 2 (ignoring VAGUE)
+				relation = Type.BEFORE;
+				break;
+
+				// gov=PRESENT, dep=... only 0-1 occurrences in training data
+			case NONE:
+			case INFINITIVE:
+			case PASSIVE:
+			case PASTPART:
+			case PRESPART:
+				break;
+			}
+			break;
+
+			// gov=... only 0-1 occurrences in training data
+		case NONE:
+		case PRESPART:
+		case FUTURE:
+		case INFINITIVE:
+		case PASSIVE:
+		case PASTPART:
+			break;
+		}
+		return relation;
+	}
+	
 	/**
 	 * No training. Just rule-based.
 	 */
